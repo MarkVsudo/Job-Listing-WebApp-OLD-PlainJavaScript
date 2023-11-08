0bcc5ad5809ad546249f0cb99ded7be5caba6bcc
Mark Veskov <mark.veskov@abv.bg>
2023-11-08 23:22:03 +0200

    Created a login page

--- routes/routes.js
+++ routes/routes.js
@@ -6,4 +6,12 @@ router.get("/", (req, res) => {
   res.render("index", { title: "JobConqueror - Homepage" });
 });
 
+router.get("/login", (req, res) => {
+  res.render("login", { title: "JobConqueror - Sign In" });
+});
+
+router.get("/register", (req, res) => {
+  res.render("register", { title: "JobConqueror - Sign Up" });
+});
+
 module.exports = router;
