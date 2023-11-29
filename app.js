24992edcfe2deb9abdf77a3618263cf4dbbf34b9
Mark Veskov <mark.veskov@abv.bg>
2023-11-29 12:29:47 +0200

    Worked on animations in home page

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// app.js
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -6,7 +5,8 @@ const bodyParser = require("body-parser");
 const dbConnection = require("./config/db-config");
 const PORT = process.env.PORT || 5000;
 
-app.use("/", require("./routes/routes"));
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
 
 // Set up EJS as the view engine
 app.set("view engine", "ejs");
@@ -14,7 +14,9 @@ app.set("views", path.join(__dirname, "views"));
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.use("/", require("./routes/routes"));
 
 // Start your Express server
 app.listen(PORT, () => {
---
+++ public/js/animations.js
@@ -0,0 +1,123 @@
+gsap.registerPlugin(ScrollTrigger, TextPlugin);
+
+gsap.from(
+  ["#categories h2", "#categories h4", ".categories-block", ".btn-categories"],
+  {
+    y: -50,
+    opacity: 0,
+    stagger: 0.3,
+    duration: 1,
+    scrollTrigger: {
+      trigger: "#categories",
+      start: "top-=200px center",
+    },
+  }
+);
+
+gsap.from(".process-sub", {
+  x: 100,
+  opacity: 0,
+  stagger: 0.3,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "#process",
+    start: "top-=200px center",
+  },
+});
+
+gsap.from(
+  [
+    "#learnFromExperts h2",
+    "#learnFromExperts h4",
+    ".experts-main-img",
+    ".experts-tutorial",
+  ],
+  {
+    y: -50,
+    opacity: 0,
+    stagger: 0.3,
+    duration: 1,
+    scrollTrigger: {
+      trigger: "#learnFromExperts",
+      start: "top-=200px center",
+    },
+  }
+);
+
+gsap.from("#mission h2", {
+  y: -50,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#mission",
+    start: "top center",
+  },
+});
+
+gsap.from("#mission .mission-text", {
+  x: -50,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#mission",
+    start: "top center",
+  },
+});
+
+gsap.from("#mission .mission-img", {
+  x: 50,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#mission",
+    start: "top center",
+  },
+});
+
+gsap.from("#vocation .vocation-text", {
+  x: -50,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#vocation",
+    start: "top center",
+  },
+});
+
+gsap.from("#vocation img", {
+  x: 50,
+  opacity: 0,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#vocation",
+    start: "top center",
+  },
+});
+
+gsap.to(".first-stat", {
+  text: {
+    value: 7856,
+    speed: 0.2,
+    stagger: 0.2,
+  },
+  ease: "linear",
+  roundProps: { innerHTML: 1 },
+});
+
+gsap.to(".second-stat", {
+  text: {
+    value: 2004,
+    speed: 0.2,
+    stagger: 0.2,
+  },
+  ease: "linear",
+  roundProps: { innerHTML: 1 },
+});
+
+gsap.to([".home-ellipse-bg", ".ellipse-categories-bg"], {
+  y: 20,
+  duration: 1.5,
+  ease: "power1.inOut",
+  yoyo: true,
+  repeat: -1,
+});
