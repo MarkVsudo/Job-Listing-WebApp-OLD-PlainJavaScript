c569e57c356d61dde3d6d3d677f24ebaedee3aee
Mark Veskov <mark.veskov@abv.bg>
2024-02-03 18:45:37 +0200

    Added image carousel

--- public/js/animations.js
+++ public/js/animations.js
@@ -146,4 +146,11 @@ window.addEventListener("scroll", function () {
   }
 });
 
+// Enabling tooltips in bootstrap
 
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
